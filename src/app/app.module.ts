c507f31e63bd896c87442510f0d3dd596016208d
nidhal <nidhal.jelassi@fsegt.utm.tn>
2023-02-07 16:10:46 +0100

    accounts exercice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,13 @@ import { CvComponent } from './cv/cv.component';
 import { ListeComponent } from './liste/liste.component';
 import { DetailsComponent } from './details/details.component';
 import { ItemComponent } from './item/item.component';
+import { HomeAccountsComponent } from './accounts/home-accounts/home-accounts.component';
+import { ItemAccountComponent } from './accounts/item-account/item-account.component';
+import { AddAccountComponent } from './accounts/add-account/add-account.component';
+import { DirectComponent } from './direct/direct.component';
 
 @NgModule({
-  declarations: [AppComponent, FirstComponent, CardComponent, ChildComponent, CvComponent, ListeComponent, DetailsComponent, ItemComponent],
+  declarations: [AppComponent, FirstComponent, CardComponent, ChildComponent, CvComponent, ListeComponent, DetailsComponent, ItemComponent, HomeAccountsComponent, ItemAccountComponent, AddAccountComponent, DirectComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
